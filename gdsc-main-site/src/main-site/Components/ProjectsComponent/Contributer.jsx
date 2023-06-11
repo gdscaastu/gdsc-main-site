b88cdf713c4c8562ebfd603255ad1fc77c0f67f8
Aser Hailu <aserhailu2020@gmail.com>
2023-06-11 12:26:00 +0300

    some changes

--- gdsc-main-site/src/main-site/Components/ProjectsComponent/Contributer.jsx
+++ gdsc-main-site/src/main-site/Components/ProjectsComponent/Contributer.jsx
@@ -2,12 +2,12 @@ import react from "react";
 import Card from "react-bootstrap/Card";
 import user from "../../../assets/Sample_User_Icon.png";
 import "../../../index.css";
-
+import { Link } from "react-router-dom";
 const Contributer = ({ contributer }) => {
   return (
     <Card
       style={{
-        width: "18rem",
+        width: "15rem",
         boxShadow: "10px 10px 5px 0px rgb(224, 222, 222)",
       }}
       className="mx-5 mb-5 ">
@@ -25,26 +25,27 @@ const Contributer = ({ contributer }) => {
       <Card.Body>
         <Card.Title className="my-1 p-2">{contributer.name}</Card.Title>
         <Card.Text className="my-3 p-2">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+         <span className="text-blue-400">Status: </span>{contributer.status}<br />
+         <span className="text-blue-400">Role: </span>{contributer.role}<br />
+
         </Card.Text>
       </Card.Body>
       <div
         className="container mb-3 flex flex-wrap justify-around">
+        <Link
+          style={{
+            color: "red",
+            fontSize: "25px",
+          }}
+          to={`https://twitter.com/${contributer.telegram_url}`}>
+          <i class="fab fa-twitter social-media-icon"></i>
+        </Link>
         <a
           style={{
             color: "red",
             fontSize: "25px",
           }}
-          href={contributer.telegram_url}>
-          <i class="fab fa-telegram"></i>
-        </a>
-        <a
-          style={{
-            color: "red",
-            fontSize: "25px",
-          }}
-          href={contributer.github_url}>
+          href={`https://github.com/${contributer.github_url}`}>
           <i class="fab fa-github"></i>
         </a>
         <a
@@ -52,7 +53,7 @@ const Contributer = ({ contributer }) => {
             color: "red",
             fontSize: "25px",
           }}
-          href={contributer.linkedin_url}>
+          href={`https://linkedin.com/in/${contributer.linkedin_url}`}>
           <i class="fab fa-linkedin-in social-media-icon"></i>
         </a>
       </div>
